818c389fff5d67ec162218c09534a705e5c0f1f8
Oji Nnorom <oji.nnorom@scotiabank.com>
2017-09-04 18:49:49 -0400

    setting git info

--- setup.sh
+++ setup.sh
@@ -1,7 +1,12 @@
 #!/usr/bin/env bash
+$git_server_url = 'github.com' 
+$repo = 'puppet'
+$repo_owner = 'onnorom'
+
 mkdir -p /etc/puppet/code
 cp -R modules /etc/puppet/code
 cp -R * /etc/puppet
 pushd /etc/puppet && git init
+git remote add origin git@${git_server_url}:${repo_owner}/${repo}.git
 
 puppet apply /etc/puppet/manifests/site.pp
