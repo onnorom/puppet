b9d9742be17599cb5e763c83dcf9404e5bc08af9
Oji Nnorom <oji.nnorom@scotiabank.com>
2017-09-04 18:54:55 -0400

    setup fix

--- setup.sh
+++ setup.sh
@@ -1,11 +1,11 @@
 #!/usr/bin/env bash
-$git_server_url = 'github.com' 
-$repo = 'puppet'
-$repo_owner = 'onnorom'
+git_server_url='github.com' 
+repo='puppet'
+repo_owner='onnorom'
 
 mkdir -p /etc/puppet/code
 cp -R modules /etc/puppet/code
-cp -R * /etc/puppet
+
 pushd /etc/puppet && git init
 git remote add origin git@${git_server_url}:${repo_owner}/${repo}.git
 
